58e5d8f5475cbd0876ac00779b7179ea35570635
dark0dave <dark0dave@mykolab.com>
2026-05-24 14:33:46 +0100

    feat(all): add realistic ruven all
    Signed-off-by: dark0dave <dark0dave@mykolab.com>

--- scripts/retraify.sh
+++ scripts/retraify.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-weidu --nogame --untraify-tra languages/english/weidu.tra --untraify-d setup-bmpp.tp2
-sed -i 's/ \/\*.*\*\///g' setup-bmpp.tp2
-weidu --nogame --traify-comment --traify-old-tra languages/english/weidu.tra --traify setup-bmpp.tp2
-mv setup-bmpp.tra languages/english/weidu.tra
+weidu --nogame --untraify-tra bmpp/languages/english/weidu.tra --untraify-d bmpp/setup-bmpp.tp2
+sed -i 's/ \/\*.*\*\///g' bmpp/setup-bmpp.tp2
+weidu --nogame --traify-comment --traify-old-tra bmpp/languages/english/weidu.tra --traify bmpp/setup-bmpp.tp2
+mv bmpp/setup-bmpp.tra bmpp/languages/english/weidu.tra
